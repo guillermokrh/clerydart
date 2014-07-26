9c7a3abd60c7a17e92dfc1702b9dce5deff581c1
guillermokrh <guillermo.krh@gmail.com>
2014-07-26 16:27:29 -0400

    Add prints_variables.py, a Python script that prints out the names of the marker variables created by Folium in the clerydart.html file created by clerydart_template.py

---
+++ clerydart_eworkspace/clerydart_dev/drivers/prints_variables.py
@@ -0,0 +1,25 @@
+#===============================================================================
+# prints_variables.py
+# Auteur: Guillermo K. Rojas
+# Prints the name of marker variables for the CleryDart .js code
+# Nasty shortcut while I figure out JavaScript
+# July 26, 2014
+#===============================================================================
+
+# Static Variables
+numberOfVariables = 178
+
+# Functions
+def print_variable_names(numberOfVariables):
+    i = 1 # Index is set to 1
+    listOfVariables = [] # initialize empty list of variables
+    while i <= numberOfVariables: # loop
+        current_variable = "marker_" + str(i) # prints variable text
+        listOfVariables.append(current_variable) #appends current variable to the list of variables
+        i = i + 1 # Adds 1 to the counter
+        
+    print ', '.join(listOfVariables) # prints the list of variables without quotes, joined by a comma
+
+        
+# Function Calls
+print_variable_names(numberOfVariables)
